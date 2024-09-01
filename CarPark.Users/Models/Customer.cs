0ecfb0cb8be8ba65d437f21e230b8d84d858adf6
kubraacelik <kkbra.celik92@gmail.com>
2024-09-01 15:07:59 +0300

    Customer class created

---
+++ CarPark.Users/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace CarPark.Users.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string NameSurname { get; set; }
+        public int Age { get; set; }
+    }
+}
