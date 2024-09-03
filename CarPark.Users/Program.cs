1404ed2c94f4bb1a073f9839e642a8dfc728cd94
kubraacelik <kkbra.celik92@gmail.com>
2024-09-03 20:57:21 +0300

    localization configured in detail

--- CarPark.Users/Program.cs
+++ CarPark.Users/Program.cs
@@ -1,4 +1,10 @@
+using CarPark.Users.Resources;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Localization.Routing;
+using Microsoft.Extensions.Options;
 using Serilog;
+using System.Globalization;
+using System.Reflection;
 
 internal class Program
 {
@@ -10,13 +16,59 @@ internal class Program
         .WriteTo.File("log.txt")
         .WriteTo.Seq("http://localhost:5341/")
         .MinimumLevel.Information()
-        .Enrich.WithProperty("ApplicationName","CarPark.Users")
+        .Enrich.WithProperty("ApplicationName", "CarPark.Users")
         .Enrich.WithMachineName()
         .CreateLogger();
-         
+
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Services.AddControllersWithViews();
+
+        // uygulamanýn localization içeriklerinin Resources klasöründen okumasýný saðlar 
+        builder.Services.AddLocalization(opt => { opt.ResourcesPath = "Resources"; });
+
+        // Models'ta veri girilmeyince key deðil value deðeri gelsin istiyorum.Yani NameSurname_Value deðil "Adýnýz Soyadýnýz Zorunlu Alan" yazýsý gelsin istiyorum
+        builder.Services.AddMvc()
+            .AddViewLocalization()
+            .AddDataAnnotationsLocalization(options =>
+             {
+                  options.DataAnnotationLocalizerProvider = (type, factory) =>
+                  {
+                      var assemblyName = new AssemblyName(typeof(SharedModelResource).GetTypeInfo().Assembly.FullName);
+                      return factory.Create(nameof(SharedModelResource), assemblyName.Name);
+                  };
+            });
+
+
+        // localization ayrýntýlý bir þekilde yapýlandýrýldý
+        builder.Services.Configure<RequestLocalizationOptions>(opt =>
+        {
+            var supportedCultures = new List<CultureInfo>
+            {
+                new CultureInfo("en-US"),
+                new CultureInfo("tr-TR"),
+                new CultureInfo("fr-FR")
+            };
+
+            opt.DefaultRequestCulture = new RequestCulture("tr-TR");
+            opt.SupportedCultures = supportedCultures;
+            opt.SupportedUICultures = supportedCultures;
+
+            opt.RequestCultureProviders = new List<IRequestCultureProvider>
+            {
+                new QueryStringRequestCultureProvider(),
+                new CookieRequestCultureProvider(),
+                new AcceptLanguageHeaderRequestCultureProvider()
+            };
+
+            //opt.RequestCultureProviders = new[]
+            //{
+            //    new RouteDataRequestCultureProvider
+            //    {
+            //        Options = opt
+            //    }
+            //};
+        });
 
         //Serilog uygulamanýn servis koleksiyonuna dahil edildi
         builder.Services.AddSerilog();
@@ -35,6 +87,10 @@ internal class Program
         app.UseRouting();
 
         app.UseAuthorization();
+
+        // localization ayarlarýnýn nasýl uygulandýðý yazýldý
+        var options = app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>();
+        app.UseRequestLocalization(options.Value);
 
         app.MapControllerRoute(
             name: "default",
