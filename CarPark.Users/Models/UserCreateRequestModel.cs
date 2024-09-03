661b166eab5c73a6583e58803f7b379d1e7a8b81
kubraacelik <kkbra.celik92@gmail.com>
2024-09-03 20:54:40 +0300

    created UserCreateRequestModel

---
+++ CarPark.Users/Models/UserCreateRequestModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarPark.Users.Models
+{
+    public class UserCreateRequestModel
+    {
+        // {0} = DisplayName'e denk geliyor
+
+        [Required(ErrorMessage = "{0} is required")]
+        [DisplayName("NameSurname")]
+        public string NameSurname { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        [DisplayName("JobTitle")]
+        public string JobTitle { get; set; }
+    }
+}
