634987dc777b69bfbe62ce35dfda98a35831c675
kubraacelik <kkbra.celik92@gmail.com>
2024-09-04 20:30:01 +0300

    created cities.cs

---
+++ CarPark.Users/Models/cities.cs
@@ -0,0 +1,13 @@
+﻿using CarPark.Entities.Concrete;
+
+namespace CarPark.Users.Models
+{
+    public class cities : BaseModel
+    {
+        public string name { get; set; }
+        public string plate { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public ICollection<County> counties { get; set; }
+    }
+}
