aaa1b7c2ccdbbe47735768e97b1b19acc3242fd6
kubraacelik <kkbra.celik92@gmail.com>
2024-09-06 18:24:22 +0300

    some basic classes defined

---
+++ CarPark.Core/Models/Result.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPark.Core.Models
+{
+    public class Result
+    {
+        // Success varsayılan true yaptık
+        public Result()
+        {
+            Success = true;
+        }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+    public class GetOneResult<TEntity> : Result where TEntity : class, new()
+    {
+        public TEntity Entity { get; set; }
+    }
+    public class GetManyResult<TEntity> : Result where TEntity : class, new() 
+    {
+        public IEnumerable<TEntity> Result { get; set; }
+    }
+    public class Message
+    {
+        public string SendEmail()
+        {
+            return "Başarılı";
+        }
+    }
+}
