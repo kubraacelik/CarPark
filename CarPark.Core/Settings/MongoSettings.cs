930605cf123dd94223c5cbfb0b5fa82242789e58
kubraacelik <kkbra.celik92@gmail.com>
2024-09-06 18:25:52 +0300

    will get ConnectionString and Database in appsettings.json

---
+++ CarPark.Core/Settings/MongoSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarPark.Core.Settings
+{
+    //appsettings.json'daki ConnectionString ve Database'i alacak
+    public class MongoSettings
+    {
+        public string ConnectionString { get; set; }
+        public string Database { get; set; }
+    }
+}
