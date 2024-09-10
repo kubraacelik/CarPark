5677b3aa0c4cbc20b2ad23468ed6d7dee6631197
kubraacelik <kkbra.celik92@gmail.com>
2024-09-10 15:00:08 +0300

    MongoIdentityUser inherited

--- CarPark.Entities/Concrete/Personel.cs
+++ CarPark.Entities/Concrete/Personel.cs
@@ -1,5 +1,7 @@
-﻿using MongoDB.Bson;
+﻿using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using MongoDbGenericRepository.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,12 +10,16 @@ using System.Threading.Tasks;
 
 namespace CarPark.Entities.Concrete
 {
-    public class Personel : BaseModel
+    // CollectionName = veritabanına bu isimle kayıt olsun
+    [CollectionName("Personel")]
+    public class Personel : MongoIdentityUser
     {
-        public string UserName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string[] Roles { get; set; }
+        // Kullanıcının oluşturulma tarihi o an olarak alınır.
+        public Personel()
+        {
+            CreatedDate = DateTime.Now;
+            Status = 1;
+        }
         public PersonelContact PersonelContact { get; set; }
         public ICollection<Address> Addresses { get; set; }
         public short Status { get; set; }
